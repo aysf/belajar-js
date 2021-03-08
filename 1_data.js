96e7d4463f4327967ddca7308664bbacc41ab438
wicak <ansufw@gmail.com>
2021-03-08 14:26:32 +0100

    added stater pack

---
+++ 1_data.js
@@ -0,0 +1,12 @@
+/////////////////////////////
+// data type in javascript //
+/////////////////////////////
+
+//  Give example fundamental: number, string, boolean
+//  Give example collection: 
+//  how to check data type: 
+//  how to convert data type
+//  declare variable
+
+// in JS, there are two variable let and const
+
---
+++ app.js
@@ -0,0 +1,9 @@
+if (1===1){
+    console.log("it's True!");
+}
+
+let rating = 3;
+
+if(rating===3){
+    console.log('you are superstar')
+}
